c12d03fbd01726d95c08c8d0cd6976b3e5be9905
Deepanshu Tiwari <ddepu11@gmail.com>
2021-03-17 13:41:38 +0530

    About us heading changed

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -3,7 +3,9 @@ import React from "react";
 export default function About() {
   return (
     <div className="about">
-      <h2>This project demonstrates how we can use react router dom.</h2>
+      <h2>
+        This project demonstrates how we can use "react-router-dom" package.
+      </h2>
     </div>
   );
 }
