9b8d64b0b40b49d1c6cd770c3cce5fceba4fae02
Deepanshu Tiwari <ddepu11@gmail.com>
2021-03-17 13:38:40 +0530

    Error page styled

--- src/Components/Error.jsx
+++ src/Components/Error.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Error() {
   return (
-    <div>
-      <h1>Error</h1>
+    <div className="error">
+      <h1>Error!! 404, The Page You have requested is not found</h1>
+      <Link className="home-btn" to="/">
+        Go Back to Home
+      </Link>
     </div>
   );
 }
